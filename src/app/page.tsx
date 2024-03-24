473c322407c22411a98718b5a175d39df65231db
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-24 10:12:57 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 
-export default function Home() {
-  const { google }=require('googleapis');
-  const CLIENT_ID = '937533774641-cmislbnthip0107uhgn7p6od62s2m3me.apps.googleusercontent.com';
-  const CLIENT_SECRET = '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';
-  const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-  const REFRESH_TOKEN = '1//047sPh3D8mKVlCgYIARAAGAQSNwF-L9IrgjVlBk2oYBtIBckWz_MUB_7TZL2qXarj1cnTSASDiIwqyfwgfiX44dHbD3eSMhcej_c';
+// export default function Home() {
+//   const { google }=require('googleapis');
+//   const CLIENT_ID = '937533774641-cmislbnthip0107uhgn7p6od62s2m3me.apps.googleusercontent.com';
+//   const CLIENT_SECRET = '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';
+//   const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+//   const REFRESH_TOKEN = '1//047sPh3D8mKVlCgYIARAAGAQSNwF-L9IrgjVlBk2oYBtIBckWz_MUB_7TZL2qXarj1cnTSASDiIwqyfwgfiX44dHbD3eSMhcej_c';
 
-  const oauth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI
-  );
+//   const oauth2Client = new google.auth.OAuth2(
+//     CLIENT_ID,
+//     CLIENT_SECRET,
+//     REDIRECT_URI
+//   );
 
 
   return (
